54ba667d4b77edeb3106446155a3e3dceb21e99b
lacerdaxt <161334412+lacerdaxt@users.noreply.github.com>
2024-09-17 12:06:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Heróis Marvel',
+    'Quem é o alter ego do Homem de Ferro?',
+    'O alter ego do Homem de Ferro é Tony Stark.'
+);
+
+criaCartao(
+    'Heróis DC',
+    'Qual é o nome verdadeiro do Batman?',
+    'O nome verdadeiro do Batman é Bruce Wayne.'
+);
+
+criaCartao(
+    'Heróis Marvel',
+    'Qual é o poder do Thor?',
+    'O Thor possui o poder do trovão e é o deus do trovão.'
+);
+
+criaCartao(
+    'Heróis DC',
+    'Qual é o superpoder do Flash?',
+    'O Flash possui super velocidade.'
+);
